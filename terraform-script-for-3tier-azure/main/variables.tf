94c3c6ff5d09f114d5002b7f9c2627b2033beb5d
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-07-08 13:32:39 +0530

    Update variables.tf

--- terraform-script-for-3tier-azure/main/variables.tf
+++ terraform-script-for-3tier-azure/main/variables.tf
@@ -23,26 +23,6 @@ variable "action_group_shortname" {
   description = "Provide the short name for Azure Action Group"
 }
 
-variable "monitoring_namespace" {
-  type = string
-  description = "Provide name of the Kubernetes Namespace to be created"
-}
-
-variable "k8s_management_node_rg" {
-  type = string
-  description = "Provide the Resource Group Name for Terraform-Server or k8s-management-node's vnet"
-}
-
-variable "k8s_management_node_vnet" {
-  type = string
-  description = "Provide the k8s-management-node or Terraform-Server's vnet name"
-}
-
-variable "k8s_management_node_vnet_id" {
-  type = string
-  description = "Provide the k8s-management-node or Terraform-Server's vnet id"
-}
-
 variable "env" {
   type = list
   description = "Provide the Environment for AKS Cluster"
