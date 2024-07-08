1f07259f4f8e8fba792345d47d8ac0aa5a1d743f
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-07-08 13:32:07 +0530

    Update main.tf

--- terraform-script-for-3tier-azure/main/main.tf
+++ terraform-script-for-3tier-azure/main/main.tf
@@ -6,11 +6,7 @@ module "aks" {
   kubernetes_version = var.kubernetes_version[6]
   ssh_public_key = var.ssh_public_key
   action_group_shortname = var.action_group_shortname
-  monitoring_namespace = var.monitoring_namespace
-  k8s_management_node_rg = var.k8s_management_node_rg
-  k8s_management_node_vnet = var.k8s_management_node_vnet
-  k8s_management_node_vnet_id = var.k8s_management_node_vnet_id 
-
+  
   env = var.env[0]
 
   ############################ Azure VM ################################# 
