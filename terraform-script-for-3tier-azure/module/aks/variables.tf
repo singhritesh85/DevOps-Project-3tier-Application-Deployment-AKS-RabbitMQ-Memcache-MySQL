b51b5b4a97b403330b303db39ee160a710c0f1b7
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2024-07-08 13:35:15 +0530

    Update variables.tf

--- terraform-script-for-3tier-azure/module/aks/variables.tf
+++ terraform-script-for-3tier-azure/module/aks/variables.tf
@@ -18,22 +18,6 @@ variable "action_group_shortname" {
 
 }
 
-variable "monitoring_namespace" {
-
-}
-
-variable "k8s_management_node_rg" {
-
-}
-
-variable "k8s_management_node_vnet" {
-
-}
-
-variable "k8s_management_node_vnet_id" {
-
-}
-
 variable "env" {
 
 }
